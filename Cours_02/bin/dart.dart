2062a5d17e162e0431d9a3b0f125394db368aeaa
BNHDeveloper <gussisabrin@gmail.com>
2025-11-20 19:25:05 +0100

    Cours 02 : { comments + Documentation }

---
+++ Cours_02/bin/dart.dart
@@ -0,0 +1,12 @@
+import 'package:dart/dart.dart' as dart;
+
+void main() {
+  //List<String> arguments
+  print('Hello world: ${dart.calculate()}!');
+  printDocument();
+}
+
+/// هذا تعليق يشرح عمل هذه الدالة
+void printDocument() {
+  print("function test documentation");
+}
