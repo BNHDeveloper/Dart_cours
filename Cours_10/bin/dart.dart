38f5b9774bd430f2f284b10a7d5c4b0f603975cc
BNHDeveloper <gussisabrin@gmail.com>
2025-11-22 11:09:14 +0100

    cours 10 : List

---
+++ Cours_10/bin/dart.dart
@@ -0,0 +1,7 @@
+void main() {
+//  List<String> student = ['alice', 'bob', 'charlie'];
+  List students = ['alice', 'bob', 'charlie',0];
+  for (int i = 0; i < students.length; i++) {
+    print("My name is: ${students[i]}");
+  }// List
+}
